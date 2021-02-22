775aa83081b027f10d526b7fcf58bb14c5d23c31
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2021-02-22 13:07:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-//document.querySelector(".onoffswitch-checkbox").addEventListener("click", unction);
-let checkBox = document.querySelector("#myonoffswitch").checked;
-
-function myFunction() {
-    document.getElementsByClassName("card__price")[0].innerHTML = "$199.99";
-    document.getElementsByClassName("card__price")[1].innerHTML = "$399.99";
-    document.getElementsByClassName("card__price-2")[0].innerHTML = "$249.99";
-}
+    let checkBox = document.querySelector("#myonoffswitch");
 
 
+    checkBox.addEventListener('change',function myFunction() {
 
-if (checkBox == false) {
-    myFunction();
-}
+    if (checkBox.checked) {
+        document.getElementsByClassName("card__price")[0].innerHTML = "<span class='card__price--dollar'>$</span>19.99";
+        document.getElementsByClassName("card__price")[1].innerHTML = "<span class='card__price--dollar'>$</span>39.99";
+        document.getElementsByClassName("card__price-2")[0].innerHTML = "<span class='card__price--dollar'>$</span>24.99";
+    } else {
+        document.getElementsByClassName("card__price")[0].innerHTML = "<span class='card__price--dollar'>$</span>199.99";
+        document.getElementsByClassName("card__price")[1].innerHTML = "<span class='card__price--dollar'>$</span>399.99";
+        document.getElementsByClassName("card__price-2")[0].innerHTML = "<span class='card__price--dollar'>$</span>249.99";
+    }
+
+})
